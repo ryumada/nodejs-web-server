14eb8db692a30159fe19704dba3c16e844984d0c
ryumada <rizukiryumada@gmail.com>
2022-03-12 08:20:42 +0700

    response status
    ... making the server to give response status based on the usecase

--- server.js
+++ server.js
@@ -2,23 +2,24 @@ const http = require('http');
 
 const requestListener = (request, response) => {
   response.setHeader('Content-Type', 'text/html');
-  response.statusCode = 200;
 
   const { method, url } = request;
 
   if(url === '/') {
     if(method === 'GET') {
+      response.statusCode = 200;
       return response.end('<h1>This is a homepage!</h1>');
     }
 
+    response.statusCode = 400;
     return response.end('<h1>Page cannot be accessed by other request.</h1>');
   }
 
   if(url === '/about') {
     if(method === 'GET') {
+      response.statusCode = 200;
       return response.end('<h1>Hello, this is an about page!</h1>');
     }
-
     
     if(method === 'POST') {
       let body = [];
@@ -30,13 +31,16 @@ const requestListener = (request, response) => {
       return request.on('end', () => {
         body = Buffer.concat(body).toString();
         const { name } = JSON.parse(body);
+        response.statusCode = 200;
         response.end(`<h1>Hi ${name}, this is an about page.</h1>`);
       });
     }
 
+    response.statusCode = 400;
     return response.end('<h1>Page cannot be accessed by other request.</h1>');
   }
 
+  response.statusCode = 404;
   return response.end('<h1>Page Not Found!</h1>');
 }
 
